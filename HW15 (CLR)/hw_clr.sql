3ee61cbd06f04cdbf6ca09b9fa8f784cbbd33de0
Combatella <alexey.uzikov@gmail.com>
2023-02-20 21:25:26 +0500

    fix

--- HW15 (CLR)/hw_clr.sql
+++ HW15 (CLR)/hw_clr.sql
@@ -4,7 +4,9 @@
 
 declare @expressionlat nvarchar(max)
 declare @expressionlon nvarchar(max)
-declare @InvoiceID intdeclare @json nvarchar(max)
+declare @InvoiceID int
+declare @json nvarchar(max)
+
 set @expressionlat = N'(latitude)(\D[^-])(-?\d{2,3}[.]\d+)'
 set @expressionlon = N'(Longitude)(\D[^-])(-?\d{2,3}[.]\d+)'
 
@@ -16,11 +18,23 @@ Latitude nvarchaR(128),
 Longitude nvarchaR(128)
 )
 
-declare mycursor cursorfor select InvoiceId, ReturnedDeliveryData from Sales.Invoicesopen mycursorfetch next from mycursor into @InvoiceID, @jsonwhile @@FETCH_STATUS = 0begininsert into #tmptable values 
+declare mycursor cursor
+for select InvoiceId, ReturnedDeliveryData from Sales.Invoices
+open mycursor
+fetch next from mycursor into @InvoiceID, @json
+while @@FETCH_STATUS = 0
+begin
+
+insert into #tmptable values 
 (
 @InvoiceID,
 SQL#.RegEx_CaptureGroup(@json, @expressionlat, 3, 'Undefined', 1, -1, 'IgnoreCase'),
 SQL#.RegEx_CaptureGroup(@json, @expressionlon, 3, 'Undefined', 1, -1, 'IgnoreCase')
 )
 
-fetch next from mycursor into @InvoiceID, @jsonendclose mycursordeallocate mycursorselect * from #tmptable
+fetch next from mycursor into @InvoiceID, @json
+end
+close mycursor
+deallocate mycursor
+
+select * from #tmptable
