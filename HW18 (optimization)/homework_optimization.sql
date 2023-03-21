02cd772a90f38db074fc3ff67d43d2b25574622e
Combatella <alexey.uzikov@gmail.com>
2023-03-21 18:16:21 +0500

    hw18

--- HW18 (optimization)/homework_optimization.sql
+++ HW18 (optimization)/homework_optimization.sql
@@ -1,4 +1,5 @@
-﻿Select 
+﻿-- при различных вариантах не удалось улучшить план, в основном получалось только изменять время ЦП 
+Select 
 	ord.CustomerID, 
 	det.StockItemID, 
 	SUM(det.UnitPrice), 
@@ -26,26 +27,109 @@ AND (
 		DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0
 GROUP BY ord.CustomerID, det.StockItemID
 ORDER BY ord.CustomerID, det.StockItemID
--- Показатели до оптимизации
--- (затронуто строк: 3619)
--- (затронуто строк: 33)
--- (затронута одна строка)
--- Время выполнения: 2023-02-14T21:38:22.8717711+05:00
+--(затронуто строк: 3619)
+--Таблица "StockItemTransactions". Число просмотров 1, логических чтений 0, физических чтений 0, упреждающих чтений 0, lob логических чтений 29, lob физических чтений 0, lob упреждающих чтений 0.
+--Таблица "StockItemTransactions". Считано сегментов 1, пропущено 0.
+--Таблица "OrderLines". Число просмотров 4, логических чтений 0, физических чтений 0, упреждающих чтений 0, lob логических чтений 331, lob физических чтений 0, lob упреждающих чтений 0.
+--Таблица "OrderLines". Считано сегментов 2, пропущено 0.
+--Таблица "Worktable". Число просмотров 0, логических чтений 0, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+--Таблица "CustomerTransactions". Число просмотров 5, логических чтений 261, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+--Таблица "Orders". Число просмотров 2, логических чтений 822, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+--Таблица "Invoices". Число просмотров 1, логических чтений 44525, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+--Таблица "StockItems". Число просмотров 1, логических чтений 2, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
 
---Время выполнения клиента	21:43:29		21:43:19		21:38:22		
---Статистика по профилю запроса							
---  Количество инструкций INSERT, DELETE и UPDATE	0		0		0		0.0000
---  Строки, изменяемые инструкциями INSERT, DELETE и UPDATE	0		0		0		0.0000
---  Количество инструкций SELECT 	5		5		4		4.6667
---  Строк, возвращенных инструкциями SELECT	3655		3655		3654		3654.6670
---  Количество транзакций 	0		0		0		0.0000
---Сетевая статистика							
---  Количество циклов обращения к серверу	5		5		4		4.6667
---  TDS-пакетов отправлено клиентом	5		5		4		4.6667
---  TDS-пакетов получено с сервера	69		69		68		68.6667
---  байтов отправлено клиентом	2204		2204		2146		2184.6670
---  байтов получено с сервера	264836		264834		264808		264826.0000
---Статистика по времени							
---  Время обработки клиента	16		70		95		60.3333
---  Общее время выполнения	456		527		541		508.0000
---  Время ожидания при ответе сервера	440		457		446		447.6667
+-- Время работы SQL Server:
+--   Время ЦП = 406 мс, затраченное время = 521 мс.
+
+
+-- можно ограничить индексы
+Select 
+	Inv.BillToCustomerID, 
+	ord.CustomerID, 
+	det.StockItemID, 
+	SUM(det.UnitPrice), 
+	SUM(det.Quantity), 
+	COUNT(ord.OrderID)
+FROM Sales.Orders AS ord
+JOIN Sales.OrderLines AS det  ON det.OrderID = ord.OrderID 
+JOIN Sales.Invoices AS Inv ON Inv.OrderID = ord.OrderID
+JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = Inv.InvoiceID
+JOIN Warehouse.StockItemTransactions AS ItemTrans ON ItemTrans.StockItemID = det.StockItemID
+join Warehouse.StockItems as si on si.StockItemID = det.StockItemID
+WHERE 
+	(Inv.BillToCustomerID = 1 or Inv.BillToCustomerID = 401) 
+	and ord.CustomerID != 1 
+	and ord.CustomerID != 401
+	and si.SupplierID = 12
+	AND (
+		SELECT SUM(Total.UnitPrice*Total.Quantity) FROM Sales.OrderLines AS Total
+		Join Sales.Orders AS ordTotal On ordTotal.OrderID = Total.OrderID
+		WHERE ordTotal.CustomerID = Inv.CustomerID
+		) > 250000
+	AND DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0
+GROUP BY Inv.BillToCustomerID, ord.CustomerID, det.StockItemID
+ORDER BY ord.CustomerID, det.StockItemID
+--(затронуто строк: 3619)
+--Таблица "StockItemTransactions". Число просмотров 1, логических чтений 0, физических чтений 0, упреждающих чтений 0, lob логических чтений 29, lob физических чтений 0, lob упреждающих чтений 0.
+--Таблица "StockItemTransactions". Считано сегментов 1, пропущено 0.
+--Таблица "OrderLines". Число просмотров 4, логических чтений 0, физических чтений 0, упреждающих чтений 0, lob логических чтений 331, lob физических чтений 0, lob упреждающих чтений 0.
+--Таблица "OrderLines". Считано сегментов 2, пропущено 0.
+--Таблица "Worktable". Число просмотров 0, логических чтений 0, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+--Таблица "CustomerTransactions". Число просмотров 5, логических чтений 261, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+--Таблица "Orders". Число просмотров 2, логических чтений 822, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+--Таблица "Invoices". Число просмотров 2, логических чтений 136318, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+--Таблица "StockItems". Число просмотров 1, логических чтений 2, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+
+-- Время работы SQL Server:
+--   Время ЦП = 297 мс, затраченное время = 377 мс.
+
+SELECT 
+	ord.CustomerID,
+	det.StockItemID,
+	SUM(det.UnitPrice),
+	SUM(det.Quantity),
+	COUNT(ord.OrderID)
+FROM Sales.Orders AS ord WITH (FORCESEEK)
+INNER JOIN Sales.OrderLines AS det ON det.OrderID = ord.OrderID
+INNER JOIN Sales.Invoices AS Inv WITH (INDEX (0))
+ON Inv.BillToCustomerID != ord.CustomerID
+	AND DATEDIFF (dd , Inv.InvoiceDate , ord.OrderDate) = 0
+	AND Inv.OrderID = ord.OrderID
+INNER JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = Inv.InvoiceID
+INNER JOIN Warehouse.StockItemTransactions AS ItemTrans ON ItemTrans.StockItemID = det.StockItemID
+WHERE (
+	SELECT SupplierId FROM Warehouse.StockItems AS It
+    Where It.StockItemID = det.StockItemID
+	) = 12
+AND (
+	SELECT SUM(Total.UnitPrice * Total.Quantity)
+	FROM Sales.OrderLines AS Total, Sales.Orders AS ordTotal
+	WHERE ordTotal.CustomerID = Inv.CustomerID AND ordTotal.OrderID = Total.OrderID
+	) > 250000
+GROUP BY ord.CustomerID, det.StockItemID
+ORDER BY ord.CustomerID, det.StockItemID
+--Время ЦП чутка лучше
+
+SELECT 
+	ord.CustomerID, 
+	det.StockItemID, 
+	SUM(det.UnitPrice), 
+	SUM(det.Quantity), 
+	COUNT(ord.OrderID) 
+FROM Sales.Orders AS ord 
+JOIN Sales.OrderLines AS det ON det.OrderID = ord.OrderID 
+JOIN Sales.Invoices AS Inv ON Inv.BillToCustomerID != ord.CustomerID
+	AND DATEDIFF (dd , Inv.InvoiceDate , ord.OrderDate) = 0 
+	AND Inv.OrderID = ord.OrderID 
+JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = Inv.InvoiceID 
+JOIN Warehouse.StockItemTransactions AS ItemTrans ON ItemTrans.StockItemID = det.StockItemID 
+WHERE (
+	SELECT SupplierId FROM Warehouse.StockItems AS It Where It.StockItemID = det.StockItemID
+	) = 12 
+AND (
+	SELECT SUM(Total.UnitPrice * Total.Quantity) FROM Sales.OrderLines AS Total, Sales.Orders AS ordTotal 
+	WHERE ordTotal.CustomerID = Inv.CustomerID AND ordTotal.OrderID = Total.OrderID
+	) > 250000 
+GROUP BY ord.CustomerID, det.StockItemID 
+ORDER BY ord.CustomerID, det.StockItemID 
+OPTION (FORCE ORDER, MAXDOP 1)
